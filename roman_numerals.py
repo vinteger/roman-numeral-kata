3e7e3de4a43cc807faa7dbb638d3cd431e73fe9c
Vaughn Walker <vaughn.walker@integral.io>
2019-06-13 16:20:12 -0400

    Refactor failing 4 test

--- roman_numerals.py
+++ roman_numerals.py
@@ -3,7 +3,10 @@ class RomanNumerals:
     def encode(self, number):
         result = ''
         i = 0
-        while i < number:
-            result += 'I'
-            i += 1
+        if number < 4:
+            while i < number:
+                result += 'I'
+                i += 1
+        if number is 4:
+            result = 'IV'
         return result
