3b24aa59c4306d60bdf240964703971ce8606a50
Vaughn Walker <vaughn.walker@integral.io>
2019-06-13 15:50:53 -0400

    Add gitignore and 3 tests

---
+++ roman_numerals.py
@@ -0,0 +1,7 @@
+class RomanNumerals:
+
+    def encode(self, numbers):
+        if numbers is 1:
+            return 'I'
+
+        return ''
---
+++ roman_numerals_test.py
@@ -0,0 +1,18 @@
+import unittest
+
+from roman_numerals import RomanNumerals
+
+
+class EncodeTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.roman_numerals = RomanNumerals()
+
+    def test_zero_returns_empty_string(self):
+        self.assertEqual(self.roman_numerals.encode(0), '')
+
+    def test_1_returns_I(self):
+        self.assertEqual(self.roman_numerals.encode(1), 'I')
+
+    def test_2_returns_II(self):
+        self.assertEqual(self.roman_numerals.encode(2), 'II')
