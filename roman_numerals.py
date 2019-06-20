1a0d0bba2914c4f9b920443bb2eb084978fb799d
Vaughn Walker <vaughn.walker@integral.io>
2019-06-20 17:36:58 -0400

    Add Tests for 7

--- roman_numerals.py
+++ roman_numerals.py
@@ -2,16 +2,26 @@ class RomanNumerals:
 
     def encode(self, number):
         result = ''
+        one = 'I'
         five = 'V'
+
+        if number is 5:
+            result = five
+
         i = 0
         if number < 4:
             while i < number:
-                result += 'I'
+                result += one
                 i += 1
+
         if number is 4:
-            result = 'I' + five
-        if number is 5:
-            result = five
+            result = one + five
+
         if number > 5:
-            result = five + 'I'
+            i = 5
+            result = five
+            while i < number:
+                result += one
+                i += 1
+
         return result
--- roman_numerals_test.py
+++ roman_numerals_test.py
@@ -9,22 +9,25 @@ class EncodeTestCase(unittest.TestCase):
         self.roman_numerals = RomanNumerals()
 
     def test_zero_returns_empty_string(self):
-        self.assertEqual(self.roman_numerals.encode(0), '')
+        self.assertEqual('', self.roman_numerals.encode(0))
 
     def test_1_returns_I(self):
-        self.assertEqual(self.roman_numerals.encode(1), 'I')
+        self.assertEqual('I', self.roman_numerals.encode(1))
 
     def test_2_returns_II(self):
-        self.assertEqual(self.roman_numerals.encode(2), 'II')
+        self.assertEqual('II', self.roman_numerals.encode(2))
 
     def test_3_returns_III(self):
-        self.assertEqual(self.roman_numerals.encode(3), 'III')
+        self.assertEqual('III', self.roman_numerals.encode(3))
 
     def test_4_returns_IV(self):
-        self.assertEqual(self.roman_numerals.encode(4), 'IV')
+        self.assertEqual('IV', self.roman_numerals.encode(4))
 
     def test_5_returns_V(self):
-        self.assertEqual(self.roman_numerals.encode(5), 'V')
+        self.assertEqual('V', self.roman_numerals.encode(5))
 
     def test_6_returns_VI(self):
-        self.assertEqual(self.roman_numerals.encode(6), 'VI')
+        self.assertEqual('VI', self.roman_numerals.encode(6))
+
+    def test_7_returns_VII(self):
+        self.assertEqual('VII', self.roman_numerals.encode(7))
